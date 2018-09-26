ef3c9f5dc1433b36ba386795b22e34bfea5ddbbc
Robert Farb <farb.robert@gmail.com>
2018-09-26 15:01:18 -0700

    Complete vanilla dom select-all challenge

--- languages/js_select_challenge/main.js
+++ languages/js_select_challenge/main.js
@@ -1,12 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
   let selectAll = document.getElementById("select-all");
   let checkBoxes = document.querySelectorAll(".city");
-  console.log(checkBoxes)
-
-  selectAll.addEventListener("click", (e) => {
-    console.log(selectAll.getAttribute("checked"))
-    checkBoxes.forEach(checkBox => {
-      checkBox.setAttribute("checked", true);
-    });
-  });
 })
