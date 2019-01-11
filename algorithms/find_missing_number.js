884f2c893a935e8de17320c01d38ef3da44b2e7d
Robert Farb <farb.robert@gmail.com>
2019-01-11 11:01:38 -0800

    Complete find_missing_num

--- algorithms/find_missing_number.js
+++ algorithms/find_missing_number.js
@@ -1,3 +1,10 @@
-function findMissingNumber(arr) {
-  
+function findMissingNumber(arr1, arr2) {
+  arr1.forEach(num => {
+    if (arr2.includes(num) === false) {
+      return num;
+    }
+  });
 }
+
+console.log(findMissingNumber([1, 2, 3, 4, 5, 6], [6, 1, 4, 2, 5]));
+
